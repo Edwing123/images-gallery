c0aaa8318780ee3a85b6842f30274b10a7314e98
Edwing123 <egarciavalle2014@gmail.com>
2021-03-26 21:21:12 -0600

    Turn Search component input into a controlled element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/Navigation";
 import Search from "./components/Search";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const searchOnSubmit = (event) => {
+    // Prevent default behavior
+    event.preventDefault();
+    console.log(query)
+  };
+
   return (
     <>
       <header className="mb-4">
         <Navigation title="Images Gallery" />
       </header>
-      <Search />
+      <Search onSubmit={searchOnSubmit} setQuery={setQuery} query={query} />
     </>
   );
 }
-
-console.log(App())
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ onSubmit, query, setQuery }) => {
   return (
     <Container fluid>
       <Row className="justify-content-center">
         <Col sm={10} md={8} lg={5}>
-          <Form>
+          <Form onSubmit={onSubmit}>
             <Form.Row className="justify-content-center">
               <Col>
-                <Form.Control placeholder="Cats, dogs, houses ..." />
+                <Form.Control
+                  type="text"
+                  placeholder="Search something ..."
+                  name="query"
+                  autoComplete="off"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                />
               </Col>
               <Col xs={"auto"}>
                 <Button variant="primary" type="submit">
